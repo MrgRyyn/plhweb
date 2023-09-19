3f295d25600072f97396d46c1ea382bd7d5083d5
MrgRyyn <rayyan.gunawan@gmail.com>
2023-09-19 10:34:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'popular-categories.html', 'test.html', 'main.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl (12, 888, 59%)', 
+        brightRedLight: 'hsl (12, 888, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)', 
+        darkBlue: 'hsl (228, 39%, 23%)',
+        darkGrayishBlue: 'hsl (227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%) ',
+        veryPaleRed: 'hs1(13, 100%, 96%) ',
+        veryLightGray: 'hsl(0, 08, 98%)',
+        lightbaige: '#FEF7EF'
+      }
+    },
+  },
+  plugins: [],
+}
+
